e262c0c21f03757e5eb73d1ac811f6c26d2f163c
Valentino <fariasvalentino25@gmail.com>
2024-08-15 20:19:25 -0300

    menu responsive

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,31 +1,45 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { useState } from "react";
+import { Grid, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-import styles from "./menu.module.scss"
-import logo from "@assets/image/logoNegro.png"
+import styles from "./menu.module.scss";
+import logo from "@assets/image/logoNegro.png";
 
-export default function Menu(){
-    return (
-        <Grid container xs={12} className={styles.menuContainer}>
-            <Grid item xs={2} sm={2} md={2} className={styles.navbarLogoContainer}>
-                <img src={logo} alt="logo" className={styles.image} />
+export default function Menu () {
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(true);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+    <Grid container className={styles.menuContainer} alignItems="center">
+      <Grid item xs={12} sm={12} md={2} className={styles.navbarLogoContainer}>
+        <img src={logo} alt="logo" className={styles.image} />
+      </Grid>
+      <Grid item xs={12} sm={12} md={10} className={styles.navBarOptionsContainer}>
+        <IconButton className={styles.navbarToggler} onClick={toggleNav}>
+          <MenuIcon />
+        </IconButton>
+        <nav className={`${styles.navbarCollapse} ${isNavOpen ? styles.show : ""}`}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12} md={8} className={styles.navbarContainer}>
+              <ul className={styles.navbarNav}>
+                <li className={styles.navItem}><a href="#" className={styles.navLink}>Inicio</a></li>
+                <li className={styles.navItem}><a href="#" className={styles.navLink}>Nosotros</a></li>
+                <li className={styles.navItem}><a href="#" className={styles.navLink}>Categoría</a></li>
+                <li className={styles.navItem}><a href="#" className={styles.navLink}>Contacto</a></li>
+              </ul>
             </Grid>
-            <Grid container xs={10} sm={10} md={10} className={styles.navBarOptionsContainer}>
-                <Grid item xs={10} sm={10} md={5} className={styles.navbarContainer}>
-                    <ul className={styles.navbarNav}>
-                        <li className={styles.navItem}><a href="#" className={styles.navLink}>Inicio</a></li>
-                        <li className={styles.navItem}><a href="#" className={styles.navLink}>Nosotros</a></li>
-                        <li className={styles.navItem}><a href="#" className={styles.navLink}>Categoria</a></li>
-                        <li className={styles.navItem}><a href="#" className={styles.navLink}>Contacto</a></li>
-                    </ul>
-                </Grid>
-                <Grid xs={10} sm={10} md={5} item  className={styles.navbarContainerUser}>
-                    <ul className={styles.navbarNav}>
-                        <li className={styles.navItem}><a href="#" className={styles.navLink}>Iniciar sesion</a></li>
-                        <li className={styles.navItem}><a href="#" className={styles.navLink}>Registrarse</a></li>
-                    </ul>
-                </Grid>
+            <Grid item xs={12} md={4} className={styles.navbarContainerUser}>
+              <ul className={styles.navbarNav}>
+                <li className={styles.navItem}><a href="#" className={styles.navLink}>Iniciar sesión</a></li>
+                <li className={styles.navItem}><a href="#" className={styles.navLink}>Registrarse</a></li>
+              </ul>
             </Grid>
-        </Grid>
-    );
-}
+          </Grid>
+        </nav>
+      </Grid>
+    </Grid>
+  );
+};
